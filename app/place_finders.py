b0ff9bc7190f8942dc164b7853ab23bd555582bd
lawrencebenson <lawrence.benson@student.hpi.de>
2015-09-03 21:45:53 +0200

    Fix encoding error

---
+++ app/place_finders.py
@@ -0,0 +1,48 @@
+from flask.json import JSONEncoder
+from googleplaces import GooglePlaces, types
+
+from api_key import API_KEY
+
+
+class NonASCIIJSONEncoder(JSONEncoder):
+    def __init__(self, **kwargs):
+        super(NonASCIIJSONEncoder, self).__init__(**kwargs)
+        self.ensure_ascii = False
+
+
+def _get_photo(place):
+    pass
+
+
+def _is_open(place_details):
+    try:
+        return place_details['opening_hours']['open_now']
+    except KeyError:
+        return False
+
+
+def _filter_place_data(place):
+    place.get_details()
+    data = {}
+    data['name'] = place.name
+    data['rating'] = place.rating
+    data['formatted_address'] = place.formatted_address
+    data['tel_number'] = place.international_phone_number
+    data['website'] = place.website
+    data['geo_location'] = place.geo_location
+
+    # Retrieve additional information
+    details = place.details
+    additionals = []
+    data.update({key: details[key] for key in additionals if key in details})
+
+    data['is_open'] = _is_open(details)
+    return data
+
+
+def find_restaurants(request):
+    g_places = GooglePlaces(API_KEY)
+    query_result = g_places.nearby_search(types=[types.TYPE_FOOD], **request)
+    places = map(_filter_place_data, query_result.places)
+
+    return {'places': places}
