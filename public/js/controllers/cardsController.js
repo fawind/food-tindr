41e97fd26bd57803e7e8cf947610f2edca45535d
fawind <windheuser.fabian@gmail.com>
2015-09-03 21:12:11 +0200

    Add swipe left/right support

--- public/js/controllers/cardsController.js
+++ public/js/controllers/cardsController.js
@@ -1,6 +1,6 @@
 angular.module('food-tinder')
-  .controller('CardsController', ['$scope', 'lodash', 'RequestService',
-    function ($scope, _, requestService) {
+  .controller('CardsController', ['$scope', '$timeout', 'lodash', 'RequestService',
+    function ($scope, $timeout, _, requestService) {
       $scope.cards = [
           {name: 'clubs', symbol: '♣'},
           {name: 'diamonds', symbol: '♦'},
@@ -8,9 +8,41 @@ angular.module('food-tinder')
           {name: 'spades', symbol: '♠'}
       ];
 
-      $scope.throwout = function (eventName, eventObject, $index) {
-          console.log('throwout', eventObject);
-          eventObject.target.remove();
+      var index = 1;
+
+      $scope.$on('stack-init', function(event, data) {
+        console.log('Stack created!');
+        $scope.stack = data;
+      });
+
+      $scope.throwoutleft = function (eventName, eventObject, $index) {
+          $timeout(function() {
+            $scope.cards.splice($scope.cards.length - 1, 1);
+          }, 200);
       };
+
+      $scope.throwoutright = function (eventName, eventObject, $index) {
+          $timeout(function() {
+            $scope.cards.splice($scope.cards.length - 1, 1);
+          }, 200);
+      };
+
+      $scope.dismiss = function() {
+        swipe(-600, -50);
+      };
+
+      $scope.favourite = function() {
+        swipe(600, -50);
+      };
+
+      function swipe(dX, dY) {
+        var cards = $scope.stack.cards;
+        if (index === cards.length + 1) {
+          return;
+        }
+
+        cards[cards.length - index].throwOut(dX, dY);
+        index++;
+      }
 
   }]);
